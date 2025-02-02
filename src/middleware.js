5af5bd163c961dc199f8257c1a8d43b78cea2e4b
Young <sinclairjang@gmail.com>
2025-02-02 16:54:47 +0900

    changed to subdomain-based url scheme

--- src/middleware.js
+++ src/middleware.js
@@ -12,32 +12,32 @@ export const config = {
 export async function middleware(request) {
     console.log("request", request.url);
     //console.log("Request Cookies:", request.cookies.getAll());
+    
     const { supabase, response } = getSupabaseReqResClient({ request });
     const { data: user, error } = await supabase.auth.getUser();
     console.log(user);
+    
     if (error) {
         console.error("Error authenticating user:", error);
     }
 
-    // refresh the token if it's expired and pass the refreshed Auth token to both the server and client
-    //const sessionUser = session.data?.session?.user;
+    const hostname = new URL(request.url).hostname;
+    const subdomain = hostname.split(".")[0];
+    console.log("Extracted Subdomain:", subdomain);
 
-    const [hostname] = getHostnameAndPort(request);
-    if (hostname in TENANT_MAP === false) {
+    if (subdomain in TENANT_MAP === false) {
         return NextResponse.rewrite(new URL("/not-found", request.url));
     } 
     
     const requestedPath = request.nextUrl.pathname;    
-    //const [tenant, ...restOfPath] = requestedPath.substr(1).split("/");
-    //const applicationPath = "/" + restOfPath.join("/");
-    const tenant = TENANT_MAP[hostname];
     const applicationPath = requestedPath;
+    const tenant = TENANT_MAP[subdomain];
     if ( !/[a-z0-9-_]+/.test(tenant) ) {
         return NextResponse.rewrite(new URL("/not-found", request.url));
     }
 
     if (applicationPath.startsWith("/safer") || applicationPath.startsWith("/hospital")) {
-        if (!user.user) {
+        if (!user?.user) {
             return NextResponse.redirect(buildUrl("/", tenant, request));
         } else if (!user.user.app_metadata?.tenants.includes(tenant)) {
             return NextResponse.rewrite(new URL("/not-found", request.url));
